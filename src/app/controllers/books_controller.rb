c368f28a5be32eaaadeaaccad8edde5bc7860b52
natsume0718 <moto718program@gmail.com>
2021-05-02 10:19:48 +0900

    flash msg

--- src/app/controllers/books_controller.rb
+++ src/app/controllers/books_controller.rb
@@ -15,8 +15,11 @@ class BooksController < ApplicationController
     book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
     @book = Book.new(book_params)
 
-    return redirect_to action: :new unless @book.save
-      
+    unless @book.save
+      flash[:danger] = "登録に失敗しました。"
+      return redirect_to action: :new 
+    end
+    flash[:success] = "家計簿に１件登録しました。"
     redirect_to book_path(@book)
   end
 
@@ -28,8 +31,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
 
-    return redirect_to action: :edit unless @book.update(book_params)
+    unless @book.update(book_params)
+      flash[:danger] = "更新に失敗しました。"
+      return redirect_to action: :edit
+    end
       
+    flash[:success] = "更新に成功しました。"
     redirect_to book_path(@book)
   end
 
@@ -37,6 +44,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.destroy
 
+    flash[:success] = "削除に成功しました。"
     redirect_to books_path # action: :index も同じ
   end
   
